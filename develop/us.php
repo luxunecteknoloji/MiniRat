4fe105525f7ee307fd24e1a2eeb65251640a4c12
Baran Kaplan <138777189+luxunecteknoloji@users.noreply.github.com>
2023-12-24 18:26:26 +0300

    Update us.php

--- develop/us.php
+++ develop/us.php
@@ -1,35 +1,27 @@
 <?php
-// MySQL bağlantısı için bilgileri güncelleyin
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "rit";
+$dbname = "database";
 
-// POST isteğinden durumu al
 $newStatus = $_POST['status'];
 
-// Başlangıçta bir $response değişkeni oluşturun
 $response = array();
 
-// MySQL bağlantısını oluştur
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Bağlantıyı kontrol et
 if ($conn->connect_error) {
     die("Veritabanı hatası: " . $conn->connect_error);
 }
 
-// Kontrol tablosundaki durumu güncelle
-$sql = "UPDATE control SET status='$newStatus' WHERE control_id=1"; // Burada "control_id" sütununu kullanın
+$sql = "UPDATE control SET status='$newStatus' WHERE control_id=1"; 
 if ($conn->query($sql) === TRUE) {
     $response['message'] = "PING GONDERILDI";
 } else {
     $response['message'] = "HATA: " . $conn->error;
 }
 
-// MySQL bağlantısını kapat
 $conn->close();
 
-// JSON formatında yanıtı döndür
 echo json_encode($response);
 ?>
