e87a3b14ea57131010a9d9c7dddad85938c71e86
Baran Kaplan <138777189+luxunecteknoloji@users.noreply.github.com>
2023-12-24 18:22:47 +0300

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,10 @@
+sayac = 0
+
+while sayac < 10:
+    print("Sakso")
+    sayac += 1
+
+    # İstediğiniz başka işlemleri buraya ekleyebilirsiniz.
+
+# Döngü tamamlandıktan sonra buraya geçer.
+print("Döngü sonlandı.")
